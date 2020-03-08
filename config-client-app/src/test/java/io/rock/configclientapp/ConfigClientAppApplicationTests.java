494c07ef82e273501f0a7827388b18a0f2cac406
Ramesh L. Patel <patel.ramesh541@gmail.com>
2020-03-08 20:09:20 +0530

    update config for spring cloud

---
+++ config-client-app/src/test/java/io/rock/configclientapp/ConfigClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rock.configclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
